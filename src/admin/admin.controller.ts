b63dadd880ffd70bd725ce59119c09f851e174bc
Pramudya Wibisono <pramudyaw.serious@gmail.com>
2024-10-10 18:41:05 +0700

    feat: add auth guard in admin & user controller

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { UpdateAdminDto } from './dto/update-admin.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { AdminAuthGuard } from 'src/auth/admin/admin-auth.guard';
 
 @Controller('admins')
 @ApiTags('Admin')
+@ApiBearerAuth()
+@UseGuards(AdminAuthGuard)
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,29 +1,36 @@
-import { Controller, Get, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { AdminAuthGuard } from 'src/auth/admin/admin-auth.guard';
+import { UserAuthGuard } from 'src/auth/user/user-auth.guard';
 
 @Controller('users')
 @ApiTags('User')
+@ApiBearerAuth()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @UseGuards(AdminAuthGuard)
   findAll() {
     return this.userService.findAll();
   }
 
   @Get(':userId')
+  @UseGuards(AdminAuthGuard, UserAuthGuard)
   findOne(@Param('userId') userId: string) {
     return this.userService.findOne(+userId);
   }
 
   @Patch(':userId')
+  @UseGuards(AdminAuthGuard, UserAuthGuard)
   update(@Param('userId') userId: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+userId, updateUserDto);
   }
 
   @Delete(':userId')
+  @UseGuards(AdminAuthGuard, UserAuthGuard)
   remove(@Param('userId') userId: string) {
     return this.userService.remove(+userId);
   }
