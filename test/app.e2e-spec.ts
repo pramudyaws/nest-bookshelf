7c7142a5c96c19a1cd3812973879be2110b3cd8c
Pramudya Wibisono <pramudyaw.serious@gmail.com>
2024-10-12 08:52:40 +0700

    test: create e2e admin & user token API tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -26,3 +26,107 @@ describe('AppController (e2e)', () => {
     await app.close();
   });
 });
+
+describe('Admin JSON Web Token (e2e)', () => {
+  let app: INestApplication;
+  let accessToken: string;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+
+    // Login using admin.test@mail.com and get admin access token
+    const loginResponse = await request(app.getHttpServer())
+      .post('/admin/auth/login')
+      .send({
+        email: 'admin.test@mail.com',
+        password: 'password',
+      });
+
+    accessToken = loginResponse.body.accessToken;
+  });
+
+  // Test positive test case by hitting admin API using valid access token
+  it('/admin/books (GET) - with valid token', () => {
+    return request(app.getHttpServer())
+      .get('/admin/books') // Try hitting admin find all books API
+      .set('Authorization', `Bearer ${accessToken}`)
+      .expect(200); // Expect success response from hitting admin API
+  });
+
+  // Test negative test case by hitting admin API using invalid access token
+  it('/admin/books (GET) - with invalid token', () => {
+    return request(app.getHttpServer())
+      .get('/admin/books') // Try hitting admin find all books API
+      .set('Authorization', 'Bearer invalidToken')
+      .expect(401); // Expect unauthorized response from hitting admin API
+  });
+
+  // Test negative test case by hitting user API using valid access token
+  it('/user/books (GET) - with valid token', () => {
+    return request(app.getHttpServer())
+      .get('/user/books') // Try hitting user find all books API
+      .set('Authorization', `Bearer ${accessToken}`)
+      .expect(401); // Expect unauthorized response from hitting user API
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+})
+
+describe('User JSON Web Token (e2e)', () => {
+  let app: INestApplication;
+  let accessToken: string;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+
+    // Login using user.test@mail.com and get user access token
+    const loginResponse = await request(app.getHttpServer())
+      .post('/user/auth/login')
+      .send({
+        email: 'user.test@mail.com',
+        password: 'password',
+      });
+
+    accessToken = loginResponse.body.accessToken;
+  });
+
+  // Test positive test case by hitting user API using valid access token
+  it('/user/books (GET) - with valid token', () => {
+    return request(app.getHttpServer())
+      .get('/user/books') // Try hitting user find all books API
+      .set('Authorization', `Bearer ${accessToken}`)
+      .expect(200); // Expect success response from hitting user API
+  });
+
+  // Test negative test case by hitting user API using invalid access token
+  it('/user/books (GET) - with invalid token', () => {
+    return request(app.getHttpServer())
+      .get('/user/books') // Try hitting user find all books API
+      .set('Authorization', 'Bearer invalidToken')
+      .expect(401); // Expect unauthorized response from hitting user API
+  });
+
+  // Test negative test case by hitting admin API using valid access token
+  it('/admin/books (GET) - with valid token', () => {
+    return request(app.getHttpServer())
+      .get('/admin/books') // Try hitting admin find all books API
+      .set('Authorization', `Bearer ${accessToken}`)
+      .expect(401); // Expect unauthorized response from hitting admin API
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+})
