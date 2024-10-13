7bde7edc0d23e2cb318469b944ea8cd4b2a22d29
Pramudya Wibisono <pramudyaw.serious@gmail.com>
2024-10-13 09:00:57 +0700

    feat: add email & phone validation in register user

--- src/auth/user/user-auth.service.ts
+++ src/auth/user/user-auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -16,6 +16,13 @@ export class UserAuthService {
     ) { }
 
     async register(createUserDto: CreateUserDto) {
+        // Validate email and phone number
+        const { email, phoneNumber } = createUserDto
+        const emailExist = await this.userRepository.exists({ where: { email: email } })
+        if (emailExist) throw new ConflictException('Email has been taken');
+        const phoneNumberExist = await this.userRepository.exists({ where: { phoneNumber: phoneNumber } })
+        if (phoneNumberExist) throw new ConflictException('Phone number has been taken');
+
         const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
         const user = this.userRepository.create({ ...createUserDto, password: hashedPassword });
         return this.userRepository.save(user);
@@ -24,19 +31,19 @@ export class UserAuthService {
     async login(loginUserDto: LoginUserDto) {
         const { email, password } = loginUserDto;
         const user = await this.userRepository.findOne({ where: { email } });
-    
+
         if (!user || !(await bcrypt.compare(password, user.password))) {
             throw new UnauthorizedException('Invalid credentials');
         }
-    
+
         user.lastLoginAt = new Date();
         await this.userRepository.save(user);
-    
+
         const accessToken = this.jwtService.sign({ id: user.id, email: user.email });
-    
+
         const { password: _, ...result } = user;
-    
+
         return { user: result, accessToken };
     }
-    
+
 }
